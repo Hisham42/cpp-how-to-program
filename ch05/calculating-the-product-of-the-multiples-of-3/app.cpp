fc8c790bb5982137d92233f564c5fca90d2f5f37
Hisham <hisham.alghaish@gmail.com>
2023-06-08 15:31:14 +0300

    Complete calculating the product of the multiples of 3

---
+++ ch05/calculating-the-product-of-the-multiples-of-3/app.cpp
@@ -0,0 +1,20 @@
+/*
+userInput
+for loop : i = 3; i <=50
+    print > 3 * i = 3i
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    unsigned int userInput;
+
+    for (unsigned int i = 3; i <= 50; i++)
+    {
+        cout << "3 * " << i << " = " << 3*i << endl;
+    }
+    
+}
