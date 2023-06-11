dc45a2d04430a442a854632dc0155e4f73190a9d
Hisham <hisham.alghaish@gmail.com>
2023-06-12 01:04:42 +0300

    Complete Guess the Number Game

---
+++ ch06/guess-the-number-game/app.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <cstdlib>
+#include <random>
+
+using namespace std;
+
+int main() {
+
+    char playAgain{'y'};
+
+    
+    
+
+    cout << "I have a number between 1 and 1000."
+        << "\n" << "Can you guess my number?";
+    
+
+    unsigned int user_input{0}, random{0};
+
+    
+    
+
+    
+
+    while (true)
+    {
+        srand(static_cast<unsigned int>(time(0)));
+        random = (1 + rand()) % 1000;
+        
+        
+        if (playAgain == 'n')
+        {
+            break;
+        }
+        
+        cout << "\n" << "Please type your first guess." << endl;
+
+        
+        
+        while (true)
+        {
+            cin >> user_input;
+
+            if (user_input == random)
+            {
+                cout << "Excellent! You guessed the number!"
+                    << "\n" << "Would you like to play again (y or n)?\n";
+
+                cin >> playAgain;
+
+                break;
+                
+
+            }
+            
+
+            if (user_input > random)
+            {
+                cout << "Too high" << endl;
+            }
+            else if (user_input < random)
+            {
+                cout << "Too low" << endl;
+            }
+            
+  
+        }
+    }
+    
+    
+}
