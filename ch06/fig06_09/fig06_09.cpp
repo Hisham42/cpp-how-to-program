225ff4be3a576341027ce98a2c76a2f57f1be7d7
Hisham <hisham.alghaish@gmail.com>
2023-06-19 03:42:00 +0300

    Complete craps game modification

--- ch06/fig06_09/fig06_09.cpp
+++ ch06/fig06_09/fig06_09.cpp
@@ -8,55 +8,90 @@ using namespace std;
 unsigned int rollDice(); // rolls dice, calculates and displays sum
 
 int main() {
+   
+   
+   
    // scoped enumeration with constants that represent the game status 
    enum class Status {CONTINUE, WON, LOST}; // all caps in constants
 
    // randomize random number generator using current time
-   srand(static_cast<unsigned int>(time(0)));
+   
 
    unsigned int myPoint{0}; // point if no win or loss on first roll
    Status gameStatus; // can be CONTINUE, WON or LOST
-   unsigned int sumOfDice{rollDice()}; // first roll of the dice
+    // first roll of the dice
+   int bankBalance{1000};
+   int wager{0};
 
-   // determine game status and point (if needed) based on first roll
-   switch (sumOfDice) {
-      case 7: // win with 7 on first roll
-      case 11: // win with 11 on first roll           
-         gameStatus = Status::WON;
-         break;
-      case 2: // lose with 2 on first roll
-      case 3: // lose with 3 on first roll
-      case 12: // lose with 12 on first roll             
-         gameStatus = Status::LOST;
-         break;
-      default: // did not win or lose, so remember point
-         gameStatus = Status::CONTINUE; // game is not over
-         myPoint = sumOfDice; // remember the point
-         cout << "Point is " << myPoint << endl;
-         break; // optional at end of switch  
-   }
 
-   // while game is not complete
-   while (Status::CONTINUE == gameStatus) { // not WON or LOST
-      sumOfDice = rollDice(); // roll dice again
-
-      // determine game status
-      if (sumOfDice == myPoint) { // win by making point
-         gameStatus = Status::WON;
+   while (bankBalance > 0)
+   {
+      
+      while (true)
+      {
+         cin >> wager;
+         if (wager <= bankBalance)
+         {
+            break;
+         }
+         
       }
-      else {
-         if (sumOfDice == 7) { // lose by rolling 7 before point
+      srand(static_cast<unsigned int>(time(0)));
+      unsigned int sumOfDice{rollDice()};
+      
+   
+   
+      // determine game status and point (if needed) based on first roll
+      switch (sumOfDice) {
+         case 7: // win with 7 on first roll
+         case 11: // win with 11 on first roll           
+            gameStatus = Status::WON;
+            
+            break;
+         case 2: // lose with 2 on first roll
+         case 3: // lose with 3 on first roll
+         case 12: // lose with 12 on first roll             
             gameStatus = Status::LOST;
+            
+            
+            break;
+         default: // did not win or lose, so remember point
+            gameStatus = Status::CONTINUE; // game is not over
+            myPoint = sumOfDice; // remember the point
+            cout << "Point is " << myPoint << endl;
+            break; // optional at end of switch  
+      }
+
+      // while game is not complete
+      while (Status::CONTINUE == gameStatus) { // not WON or LOST
+         sumOfDice = rollDice(); // roll dice again
+
+         // determine game status
+         if (sumOfDice == myPoint) { // win by making point
+            gameStatus = Status::WON;
+         }
+         else {
+            if (sumOfDice == 7) { // lose by rolling 7 before point
+               gameStatus = Status::LOST;
+            }
          }
       }
-   }
 
-   // display won or lost message
-   if (Status::WON == gameStatus) {
-      cout << "Player wins" << endl;
-   }
-   else {
-      cout << "Player loses" << endl;
+      // display won or lost message
+      if (Status::WON == gameStatus) {
+         cout << "Player wins" << endl;
+         bankBalance += wager;
+         cout << "You won $" << wager << ", your total balance is " << bankBalance << endl;
+      }
+      else {
+         cout << "Player loses" << endl;
+         bankBalance -= wager;
+         cout << "You lost $" << wager << ", your total balance is " << bankBalance << endl;
+         if (bankBalance == 0)
+         {
+            cout << "Sorry. You busted!" << endl;
+         }
+      }
    }
 }
 
