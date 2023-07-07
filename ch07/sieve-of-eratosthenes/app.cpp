5d63729f1954f0e66f65870a77a67b3af1d4b9b3
Hisham <hisham.alghaish@gmail.com>
2023-07-07 07:36:35 +0300

    Complete sieve of eratosthenes

---
+++ ch07/sieve-of-eratosthenes/app.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <iomanip>
+#include <array>
+
+using namespace std;
+
+int main() {
+
+    //initialize array size
+    int size = 1000;
+
+    //create an array
+    int primeNumbers [size];
+
+    //initiate it to 1
+    for (size_t i = 0; i < size; i++)
+    {
+        primeNumbers[i] = 1;
+    }
+
+    //algorithm
+    for (size_t j = 2; j < size; j++)
+    {
+        for (size_t k = j + 1; k < size; k++)
+        {
+            if (primeNumbers[k] == 1)
+            {
+                if (k % j == 0)
+                {
+                    primeNumbers[k] = 0;
+                }
+            }
+            
+        }
+    }
+
+    //print output
+
+    for (size_t l = 2; l < size; l++)
+    {
+        if (primeNumbers[l] == 1)
+        {
+            cout << l << endl;
+        }
+        
+        
+    }
+    
+}
