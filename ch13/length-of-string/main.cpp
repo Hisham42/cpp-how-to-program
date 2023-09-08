bb58ec6d3a3713f4c0ac5c7a10b26d2ae31cb804
Hisham <hisham.alghaish@gmail.com>
2023-09-08 03:40:57 +0300

    Complete Length of a string

---
+++ ch13/length-of-string/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+    string h;
+
+    getline(cin, h);
+
+    cout << h.length() << endl;
+    
+
+   
+
+    
+
+
+}
