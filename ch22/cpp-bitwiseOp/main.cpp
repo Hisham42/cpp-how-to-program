9fd31c95d3d057d2ae000235bcf3b41ed07ac84d
Hisham <hisham.alghaish@gmail.com>
2023-11-12 18:46:46 +0200

    Complete bitwise operators

--- ch22/cpp-bitwiseOp/main.cpp
+++ ch22/cpp-bitwiseOp/main.cpp
@@ -2,7 +2,7 @@
 #include <bitset>
 
 int main() {
-    int i = 0b10011110;
+    int i = 158;
     int j = 0b01110000;
 
     std::cout << std::bitset<8>(i) << std::endl;
@@ -14,6 +14,12 @@ int main() {
     std::cout << "^ " << std::bitset<8>(i ^ j) << std::endl;
     std::cout << "i << " << std::bitset<8>(i << 1) << std::endl;
     std::cout << "j >> " << std::bitset<8>(j >> 2) << std::endl;
-    std::cout << "~j " << std::bitset<8>(~j) << std::endl;
 
+    std::cout << "i << " << (i) << std::endl;
+    std::cout << "j >> " << (j) << std::endl;
+
+    std::cout << "i << " << (i << 1) << std::endl;
+    std::cout << "j >> " << (j >> 2) << std::endl;
+
+    std::cout << "~j " << std::bitset<8>(~j) << std::endl;
 }
