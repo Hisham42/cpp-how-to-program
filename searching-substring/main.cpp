e57b03c16e968420fab6f4accd776ad80dc1f3d1
Hisham <hisham.alghaish@gmail.com>
2023-11-15 01:14:27 +0200

    Complete Searching for Char

---
+++ searching-substring/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+
+int main() {
+
+    std::string a;
+
+    getline(std::cin, a);
+
+    std::string b;
+
+    getline(std::cin, b);
+
+    int count{0};
+    for (size_t i = 0; i < a.size(); i++)
+    {
+        if (b == a.substr(i, 5))
+        {
+            count++;
+        }
+
+    }
+    
+    std::cout << b << " is found " << count << " times." << std::endl;
+}
