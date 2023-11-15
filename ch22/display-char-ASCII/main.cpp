585a84a965bbeac0c301a744f82d7ca91b4a4131
Hisham <hisham.alghaish@gmail.com>
2023-11-16 01:12:01 +0200

    Complete Display Char ASCII

---
+++ ch22/display-char-ASCII/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <sstream>
+
+int main(int argc, char* argv[]) {
+
+    for (size_t i = 0; i < 255; i++)
+    {
+        std::cout << char(i) << std::endl;
+    }
+    
+   
+}
